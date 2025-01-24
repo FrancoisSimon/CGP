2aad7ed900e65112512934bf2b08f96b86f9816d
François Simon <87198853+FrancoisSimon@users.noreply.github.com>
2025-01-24 12:22:46 -0500

    Create Simulate_tracks.py

---
+++ Simulate_tracks.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 12:19:29 2025
+
+@author: Franc
+"""
+import numpy as np
+from numba import njit, typed
+
+@njit
+def anomalous_diff_mixture(track_len=640,
+                           nb_tracks = 100,
+                           LocErr=0.02, # localization error in x, y and z (even if not used)
+                           Fs = np.array([0.5, 0.5]),
+                           Ds = np.array([0.0, 0.05]),
+                           nb_dims = 2,
+                           velocities = np.array([0.1, 0.0]),
+                           angular_Ds = np.array([0.0, 0.0]),
+                           conf_forces = np.array([0.0, 0.2]),
+                           conf_Ds = np.array([0.0, 0.0]),
+                           conf_dists = np.array([0.0, 0.0]),
+                           LocErr_std = 0,
+                           dt = 0.02,
+                           nb_sub_steps = 10,
+                           field_of_view = np.array([10,10])):
+            
+    nb_states = len(velocities)
+    if not np.all(np.array([len(Fs), len(Ds), len(velocities), len(angular_Ds), len(conf_forces), len(conf_Ds), len(conf_dists)]) == nb_states):
+        raise ValueError('Fs, Ds, velocities, angular_Ds, conf_forces, conf_Ds and conf_dists must all be 1D arrays of the same number of element (one element per state)')
+    # diff + persistent motion + elastic confinement
+    conf_sub_forces = conf_forces / nb_sub_steps
+    sub_dt = dt / nb_sub_steps
+   
+    cum_Fs = np.zeros(nb_states)
+    cum_Fs[0] = Fs[0]
+    for state in range(1, nb_states):
+        cum_Fs[state] = cum_Fs[state-1] + Fs[state]
+    
+    all_states = np.zeros(nb_tracks)
+    
+    for kkk in range(nb_tracks):
+        state = np.argmin(np.random.rand()>cum_Fs)
+        all_states[kkk] = state
+        D, velocity, angular_D, conf_sub_force, conf_D, conf_dist = (Ds[state], velocities[state], angular_Ds[state], conf_sub_forces[state], conf_Ds[state], conf_dists[state])
+       
+        positions = np.zeros((track_len * nb_sub_steps, nb_dims))
+        
+        positions[0] = np.random.rand(nb_dims)*field_of_view
+        disps = np.random.normal(0, np.sqrt(2*D*sub_dt), ((track_len) * nb_sub_steps - 1, nb_dims))
+       
+        anchor_positions = np.random.normal(0, np.sqrt(2*conf_D*sub_dt), ((track_len) * nb_sub_steps - 1, nb_dims))
+        anchor_positions[0] = positions[0] + np.random.normal(0,conf_dist, nb_dims)
+       
+        for i in range(1, len(anchor_positions)):
+            anchor_positions[i] += anchor_positions[i-1]
+       
+        d_angles = np.random.normal(0, 1, ((track_len) * nb_sub_steps)-1) * (2*angular_D*sub_dt)**0.5
+        angles = np.zeros((track_len * nb_sub_steps-1))
+        angles[0] = np.random.rand()*2*np.pi
+        for i in range(1, len(d_angles)):
+            angles[i] = angles[i-1] + d_angles[i]
+       
+        for i in range(len(positions)-1):
+            angle = angles[i-1]
+            pesistent_disp = np.array([np.cos(angle), np.sin(angle)]).T * velocity/nb_sub_steps
+            positions[i+1] = positions[i] + pesistent_disp + disps[i]
+            positions[i+1] = (1-conf_sub_force) *  positions[i+1] + conf_sub_force * anchor_positions[i]
+       
+        final_track = np.zeros((track_len, nb_dims))
+        for i in range(track_len):
+            final_track[i] = positions[i*nb_sub_steps]
+       
+        final_track += np.random.normal(0, LocErr, (track_len, nb_dims))
+       
+        if kkk ==0:
+            final_tracks = typed.List([final_track])
+        else:
+            final_tracks.append(final_track)
+    return np.array(final_tracks), np.array(all_states)
+
+
